854f3ae46f6be9d43bb65a0bfd1984e042491de9
aleksanderdimitrov <a.dimitrov@flygms.com>
2024-12-03 18:55:48 +0200

    Accessible server

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.get('/', (req, res) => {
   });
 });
 
-// Start the server
+// Start the server on 0.0.0.0 to accept connections from any device
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
